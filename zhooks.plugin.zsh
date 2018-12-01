5656b9d1f3b47badb683bc6e318b04169239feb1
Alexandros Kozák <git-agkozak@sneakemail.com>
2018-12-01 08:32:46 -0800

    Unnecessary parameter flag

--- zhooks.plugin.zsh
+++ zhooks.plugin.zsh
@@ -54,7 +54,7 @@ zhooks() {
   for i in $hook_names; do
     # Display contents of hook arrays
     local hook_var="${i}_functions"
-    local hook_var_content="$(print -l ${(eP)hook_var})"
+    local hook_var_content="$(print -l ${(P)hook_var})"
     if [[ -n $hook_var_content ]]; then
       printf '%s:\n%s\n\n' "${start_color}${hook_var}${reset_color}" "$hook_var_content"
       (( exit_code++ ))
